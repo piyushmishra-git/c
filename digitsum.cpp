880eb2309d846faad054ddbf899f678e393342d9
piyushmishra-git <piyushmishra.github@gmail.com>
2025-09-04 15:39:33 +0530

    Describe what you changed

---
+++ digitsum.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+ 
+ int main()
+{
+    int n=2345;
+    int count=0;
+    if(n==0)
+    count =1;
+
+    while(n>0)
+    {
+        count++;
+        n=n/10;
+    }
+    printf("%d",count);
+    return 0;
+}
